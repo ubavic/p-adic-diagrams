d0ac5405bd9fa74a1c09c003c138d150dae1543d
Nikola Ubavic <mm16310@alas.matf.bg.ac.rs>
2021-04-17 14:08:44 +0200

    Code cleanup

--- src/Main.hs
+++ src/Main.hs
@@ -11,23 +11,20 @@ import Diagrams.Backend.Rasterific.CmdLine
 -- for svg output comment out previous line and uncomment following
 -- import Diagrams.Backend.SVG.CmdLine 
 
-circleColour ::  AlphaColour Double
-circleColour = withOpacity (sRGB 1 0 0) 0.1
-
-myCircle :: Diagram B
-myCircle = circle 1 # fcA circleColour # lwL 0.012
+circle' :: Int -> Diagram B
+circle' _ = circle 1 # fcA circleColour # lwL 0.012
+    where circleColour = withOpacity (sRGB 1 0 0) 0.1
 
 pAdic :: Int -> Int -> Diagram B
-pAdic 1 _  = myCircle
-pAdic n p  = myCircle `atop` (scale r circles)
-    where circles  = atPoints vertices $ repeat $ pAdic (n - 1) p
+pAdic p 1 = circle' 1
+pAdic p n = circle' n `atop` scale r circles
+    where circles  = atPoints vertices $ repeat $ pAdic p (n - 1)
           vertices = trailVertices $ regPoly p 2
           r = sin (pi / q) / (sin (pi / q) + 1)
           q = fromIntegral p
 
--- square is used for padding 
 createDiagram :: Int -> Int -> Diagram B
-createDiagram p depth  = (pAdic depth p) `atop` square 2.1 # fc white # lw none 
+createDiagram p depth = pAdic p depth # bgFrame 0.1 white
 
 main :: IO ()
 main = mainWith createDiagram
